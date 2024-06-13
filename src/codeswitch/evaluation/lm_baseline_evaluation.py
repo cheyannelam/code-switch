78ab2f1a92314559befd8829690c50b34f1de9fc
cheyannelam <cheyannelhy@gmail.com>
2024-06-13 19:02:37 +0000

    fix dataloader

--- src/codeswitch/evaluation/lm_baseline_evaluation.py
+++ src/codeswitch/evaluation/lm_baseline_evaluation.py
@@ -1,10 +1,7 @@
-import json
-import os
 import pickle  # nosec B403 B301
 import time
 
 import nltk
-import pandas as pd
 import torch
 from blender_model import OnnxBlender  # noqa
 from nltk.lm.preprocessing import pad_both_ends
@@ -17,55 +14,7 @@ from transformers import (  # noqa
     T5Tokenizer,
 )
 
-
-def check_device():
-    if torch.cuda.is_available():
-        device = "cuda"
-    elif torch.backends.mps.is_available():
-        device = "mps"
-    else:
-        device = "cpu"
-    return device
-
-
-def read_splitted_miami():
-    """return (audio_filepath_list, text_list)"""
-    with open(
-        "/home/public/data/Miami/manifests/eng/herring1.json", "r", encoding="utf-8"
-    ) as file:
-        lines = file.read().splitlines()
-    dicts = [json.loads(line) for line in lines]
-    return [(d["audio_filepath"], d["text"]) for d in dicts]
-
-
-def read_synthetic_data(data_path=""):
-    """return (audio_filepath_list, text_list)"""
-    if data_path == "":
-        data_path = os.path.join(
-            os.path.dirname(__file__), "../../../data/synthetic_code_switch_data"
-        )
-        data_path = os.path.normpath(data_path)
-
-    val_dataset = pd.read_csv(os.path.join(data_path, "output.csv"))
-    val_dataset["audio_filename"] = val_dataset["audio_filename"].apply(
-        lambda x: data_path + "/audio/" + x
-    )
-    return list(
-        zip(
-            val_dataset["audio_filename"].tolist(),
-            val_dataset["code-switched"].tolist(),
-        )
-    )
-
-
-def read_text_data(data_path=None):
-    """return a text list"""
-    if data_path is None:
-        data_path = "/home/public/data/synthetic/utterance.txt"
-        print("No specialized data_path, reading default utterance.txt dataset")
-    with open(data_path, "r", encoding="utf-8") as file:
-        lines = file.read().splitlines()
-    return lines
+from codeswitch.dataloader import check_device, read_synthetic_data
 
 
 def predict_next_token(tokenizer, logits, top_k=5):
